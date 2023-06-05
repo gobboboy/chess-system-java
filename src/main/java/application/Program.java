a8b7d983c0ceec93320936d9ba0710c8f6d4dd50
Marcos Gobbo <marcoszgobbo@gmail.com>
2023-06-05 14:31:42 -0300

    Project created

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+/**
+ *
+ * @author gobboboy
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
